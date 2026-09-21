f6b0ffa3200433ec467075a08e4551c545755098
zakarya hari <zakariahari42@gmail.com>
2026-09-21 11:28:48 +0000

    Refactor data exploration script to improve readability and maintainability

--- notebooks/data_exploration_cleaning.py
+++ notebooks/data_exploration_cleaning.py
@@ -6,3 +6,15 @@ print(df.shape)
 print(df.info())
 df = df.drop(columns=['Id'])
 print(df)
+
+# print(df.isna().sum())
+
+df["MSSubClass"] = df["MSSubClass"].astype(str)
+df["MoSold"] = df["MoSold"].astype(str)
+df["YrSold"] = df["YrSold"].astype(str)
+
+# print(df.info())
+
+missing = df.isnull().sum()
+
+print(missing[missing > 0])
