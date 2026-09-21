e0370331d6299def87c24271215bb0295d24537b
zakarya hari <zakariahari42@gmail.com>
2026-09-21 12:26:47 +0000

    Enhance data cleaning process by filling missing values with appropriate strategies

--- notebooks/data_exploration_cleaning.py
+++ notebooks/data_exploration_cleaning.py
@@ -13,8 +13,28 @@ df["MSSubClass"] = df["MSSubClass"].astype(str)
 df["MoSold"] = df["MoSold"].astype(str)
 df["YrSold"] = df["YrSold"].astype(str)
 
-# print(df.info())
-
 missing = df.isnull().sum()
+missing = missing[missing > 0]
 
-print(missing[missing > 0])
+cols_to_none = ['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'FireplaceQu', 
+                'GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 
+                'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 
+                'BsmtFinType2', 'MasVnrType']
+
+cols_to_zero = ['GarageYrBlt', 'GarageArea', 'GarageCars', 'BsmtFinSF1', 
+                'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 
+                'BsmtHalfBath', 'MasVnrArea']
+
+
+for col in missing.index:
+    if col in cols_to_none:
+        df[col] = df[col].fillna("None")
+    elif col in cols_to_zero:
+        df[col] = df[col].fillna(0)
+    elif col == "LotFrontage":
+        df[col] = df[col].fillna(df.groupby("Neighborhood")[col].transform("median"))
+    else :
+        df[col] = df[col].fillna(df[col].mode()[0])
+
+print(df.isnull().sum().sum())
+
