3d59458c89dd072e661164bd6be3f0f60c473342
zakarya hari <zakariahari42@gmail.com>
2026-09-22 17:09:57 +0000

    Refactor data loading and preprocessing in training script

--- src/train.py
+++ src/train.py
@@ -3,6 +3,16 @@ from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/processed/engineered_house_prices.csv")
 
-print(df)
+# print(df)
 
 
+y = df['SalePrice']
+x = df.drop(columns='SalePrice')
+
+print(x)
+print(y)
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+print("X_train shape:", X_train.shape)
+print("X_test shape:", X_test.shape)
