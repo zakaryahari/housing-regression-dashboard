f95a057af31f4f0c0720933bb42c10d9581b2037
zakarya hari <zakariahari42@gmail.com>
2026-09-22 17:10:18 +0000

    Add linear regression model training to the training script

--- src/train.py
+++ src/train.py
@@ -16,3 +16,8 @@ X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 
 print("X_train shape:", X_train.shape)
 print("X_test shape:", X_test.shape)
+
+
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(X_train, y_train)
