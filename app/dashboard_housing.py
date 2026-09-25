0b6b54f156d54a1dd134b67a73cab0ed9749a571
zakarya hari <zakariahari42@gmail.com>
2026-09-25 13:57:38 +0000

    Add property features and location details to the housing price dashboard

--- app/dashboard_housing.py
+++ app/dashboard_housing.py
@@ -10,3 +10,14 @@ st.set_page_config(page_title="Housing Price Predictor", layout="wide")
 model = joblib.load('./models/ridge.pkl')
 model_columns = model.feature_names_in_
 
+
+st.sidebar.header("Property Features")
+
+
+surface_area = st.sidebar.slider("Living Area (Square Feet)", min_value=500, max_value=5000, value=1500, step=50)
+
+st.sidebar.subheader("Location Details")
+stonebr = st.sidebar.selectbox("Neighborhood: Stone Brook", ["No", "Yes"])
+noridge = st.sidebar.selectbox("Neighborhood: Northridge", ["No", "Yes"])
+mitchel = st.sidebar.selectbox("Neighborhood: Mitchell", ["No", "Yes"])
+landslope = st.sidebar.selectbox("Severe Land Slope", ["No", "Yes"])
